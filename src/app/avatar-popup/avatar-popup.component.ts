1146c92a02e91494822efc46cb1d03408342b9bb
olcayusta <olcayusta10@gmail.com>
2019-12-25 14:45:00 +0300

    "1444"

--- src/app/avatar-popup/avatar-popup.component.ts
+++ src/app/avatar-popup/avatar-popup.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { User } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-avatar-popup',
   templateUrl: './avatar-popup.component.html',
-  styleUrls: ['./avatar-popup.component.scss']
+  styleUrls: ['./avatar-popup.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarPopupComponent implements OnInit {
   @Input() user: User;
--- src/app/post/pipes/trust-url.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TrustUrlPipe } from './trust-url.pipe';
-
-describe('TrustUrlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TrustUrlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
